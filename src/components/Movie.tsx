00b77d04dee5a3572c931909b6396dfa3764646e
Surbhi Midha <surbhimidha@192.168.20.66>
2022-06-28 15:59:34 +1000

    Added currency conversion function

--- src/components/Movie.tsx
+++ src/components/Movie.tsx
@@ -1,4 +1,4 @@
-import React, {FC} from "react";
+import React, {FC, useState} from "react";
 import { Link } from "react-router-dom";
 import colorizePrice from "../utils/colorizePrice";
 import ratingDisplay from "../utils/ratingDisplay";
@@ -11,6 +11,29 @@ interface IMovieInterface {
 export const Movie: FC<IMovieInterface> = ({ movie }) => {
   const cinemaWorld: IMovieData = movie?.cinemaWorld?.data?.movie
   const filmWorld: IMovieData = movie?.filmWorld?.data?.movie
+  const [priceFilmWorld, setPriceFilmWorld] = useState(filmWorld?.Price)
+  const [priceCinemaWorld, setPriceCinemaWorld] = useState(cinemaWorld?.Price)
+  // const [priceCinemaWorld, setPriceCinemaWorld] = useState(parseInt(cinemaWorld?.Price))
+  const [currencyType, setCurrencyType] = useState<string>('dollar')
+
+  const convertPrice = () => {
+    console.log('hello')
+    if(currencyType === 'pound'){
+      setPriceCinemaWorld((parseFloat(cinemaWorld.Price) * 0.56).toFixed(2))
+      setPriceFilmWorld((parseFloat(filmWorld.Price) * 0.56).toFixed(2))
+      setCurrencyType('dollar')
+    } else {
+      setPriceCinemaWorld((parseFloat(cinemaWorld.Price) * 1.77).toFixed(2))
+      setPriceFilmWorld((parseFloat(priceCinemaWorld) * 1.77).toFixed(2))
+      setCurrencyType('pound')
+    }
+    console.log('priceFilmWorld=> ',priceFilmWorld)
+    console.log('priceFilmWorld=> ',priceFilmWorld)
+  }
+
+  const addCurrType = () => {
+    return currencyType === 'dollar' ? '$' : '£'
+  }
 
   return (
     <section>
@@ -44,7 +67,7 @@ export const Movie: FC<IMovieInterface> = ({ movie }) => {
             id={colorizePrice(filmWorld?.Price, cinemaWorld?.Price)}
           >
             <p className="fw">Film World</p>
-            {filmWorld?.Price ? "$" + filmWorld?.Price : <p>N/A</p>}
+            {filmWorld?.Price ? addCurrType() + filmWorld?.Price : <p>N/A</p>}
           </a>
           <a
             href="#"
@@ -52,8 +75,9 @@ export const Movie: FC<IMovieInterface> = ({ movie }) => {
             id={colorizePrice(cinemaWorld?.Price, filmWorld?.Price)}
           >
             <p className="cw">Cinema World</p>
-            {cinemaWorld?.Price ? "$" + cinemaWorld?.Price : <p>N/A</p>}
+            {cinemaWorld?.Price ? addCurrType() + priceCinemaWorld : <p>N/A</p>}
           </a>
+        <button onClick={() => convertPrice()} className='movie-box__convertPrice' aria-label='convertPrice'>Change to {currencyType}</button>
         </div>
       </div>
     </section>
